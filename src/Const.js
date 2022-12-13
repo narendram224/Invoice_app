c212bac08fc5eda68e15502e28bb0c5b78245b42
narendram224 <narendram224.nm@gmail.com>
2022-12-14 04:26:56 +0530

    Minor changes

--- src/Const.js
+++ src/Const.js
@@ -1,5 +1,5 @@
 export const KEYS = {
   USER_ID: process.env.REACT_APP_USER_ID || ``, //userID
-  TEMPLATE_ID: process.env.REACT_APP_TEMPLATE_IDD || ``,
+  TEMPLATE_ID: process.env.REACT_APP_TEMPLATE_ID || ``,
   PUBLIC_ID: process.env.REACT_APP_PUBLIC_ID || ``, //templateID
 }
